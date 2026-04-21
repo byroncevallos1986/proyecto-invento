54c3bbb158bb61797ccc4f942df6439f4a718a7f
byroncevallos1986 <byron.cevallos1986@gmail.com>
2026-04-21 18:18:07 -0500

    INVENTO_24 corrección de versión de firestore

--- auditLog.js
+++ auditLog.js
@@ -1,5 +1,9 @@
 import { db } from "./firebase.js";
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+import {
+collection,
+addDoc
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js"; // ✅ MISMA VERSIÓN
 
 export async function registrarAuditLog(log) {
     try {
