47e55e53e90849dc186782c2690ff90fd5e741b1
byroncevallos1986 <byron.cevallos1986@gmail.com>
2026-04-24 14:12:57 -0500

    INVENTO_26 Eliminar mensaje en consola

--- auditLog.js
+++ auditLog.js
@@ -1,37 +1,15 @@
-//import { db } from "./firebase.js";
-
-//import {
-//collection,
-//addDoc
-//} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js"; // ✅ MISMA VERSIÓN
-
-//export async function registrarAuditLog(log) {
-//    try {
-//        await addDoc(collection(db, "audit_logs"), log);
-//        console.log("Audit log guardado");
-//    } catch (error) {
-//        console.error("Error audit log:", error);
-//    }
-//}
-
 import { db } from "./firebase.js";
 
 import {
-  collection,
-  addDoc
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
-console.log("🔥 auditLog cargado");
-console.log("🔥 DB IMPORTADA:", db);
+collection,
+addDoc
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js"; // ✅ MISMA VERSIÓN
 
 export async function registrarAuditLog(log) {
-  try {
-    console.log("🔥 Ejecutando audit log...");
-
-    await addDoc(collection(db, "audit_logs"), log);
-
-    console.log("✅ Audit log guardado");
-  } catch (error) {
-    console.error("❌ Error audit log:", error);
-  }
+    try {
+        await addDoc(collection(db, "audit_logs"), log);
+        //console.log("Audit log guardado");
+    } catch (error) {
+        console.error("Error audit log:", error);
+    }
 }
