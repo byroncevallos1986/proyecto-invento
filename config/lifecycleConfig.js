2b43a78929b336fb236a1c75d07ee947147510ba
byroncevallos1986 <byron.cevallos1986@gmail.com>
2026-05-02 16:42:51 -0500

    INVENTO_36 Actualizar archivo de configuracion

--- config/lifecycleConfig.js
+++ config/lifecycleConfig.js
@@ -1,4 +1,13 @@
 module.exports = {
+  /*Luego de inactivar de forma manual (front end) un usuario, deben pasar 40 días seguidos 
+  para ser anonimizado de forma automática*/
+  DIAS_ANOM: 40,
+
+  /*Luego de anomización de un usuario, deben pasar 1825 días seguidos 
+  para ser eliminado (Soft-delete) de forma automática*/
   DIAS_SOFT_DELETE: 1825,
+
+  /*Luego de ser eliminado (Soft-delete) un usuario, debe pasar 1 día 
+  para ser eliminado definitivamente (Hard-delete) de forma automática*/
   DIAS_HARD_DELETE: 1
 };
